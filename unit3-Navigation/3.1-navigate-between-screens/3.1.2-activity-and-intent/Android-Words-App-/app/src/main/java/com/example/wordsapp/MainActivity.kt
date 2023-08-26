f4b103f8eecd24b53991dd32d39c827c970ef014
sudipto <cg.sudipc@gmail.com>
2023-08-27 00:22:30 +0530

    activity lifecycle

---
+++ unit3-Navigation/3.1-navigate-between-screens/3.1.2-activity-and-intent/Android-Words-App-/app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.wordsapp
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.ActivityMainBinding
+
+/**
+ * Main Activity and entry point for the app. Displays a RecyclerView of letters.
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private var isLinearLayoutManager = true;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        // Sets the LinearLayoutManager of the recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
+    }
+
+    private fun chooseLayout(){
+        /**
+         * here you use an if statement to assign the layout-manager. In addition to setting the
+         * layout-manager, this code also assigns the adapter. LetterAdapter is used for both list and
+         * grid layouts
+         * */
+        if(isLinearLayoutManager){
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(this, 4)
+        }
+        recyclerView.adapter = LetterAdapter()
+    }
+
+    private fun setIcon(menuItem: MenuItem?){
+        if(menuItem == null){
+            return;
+        }
+        // the icon is conditionally set based on the isLinearLayoutManager property
+        menuItem.icon  =
+            if(isLinearLayoutManager){
+                ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+            } else{
+            ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_menu, menu)
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        // calls code to set the icon based on the LinearLayoutManager of the RecyclerView
+        setIcon(layoutButton)
+        return true;
+    }
+
+    /**
+     * this is called any time a menu item is tapped so you need to be sure to check which menu
+     * item is tapped. You use a when statement, above. If the id matches the action the
+     * action_switch_layout menu item, you negate the value of the isLinearLayoutManager. Then
+     * call chooseLayout() and setIcon() to update the UI accordingly.
+     * */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_switch_layout -> {
+                // Sets isLinearLayoutManager (a Boolean) to the opposite value
+                isLinearLayoutManager = !isLinearLayoutManager
+                //Sets layout and icon
+                chooseLayout()
+                setIcon(item)
+                return true
+            }
+            /**
+             * otherwise, do nothing and use the core event handling
+             *
+             * when clauses require that all possible paths be accounted for explicitly,
+             * for instance both the true and false cases if the value is Boolean,
+             * or an else to catch all the unhandled cases
+             * */
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+
+}
